16a68631149f63528d9261c841bcc4f0a3e0bad6
Brie <bbunge7@gmail.com>
2011-11-01 04:53:32 -0700

    searching and quard labels

--- js/script.js
+++ js/script.js
@@ -11,13 +11,15 @@ var moviePosterW = 60;
 var moviePosterH = 90;
 
 var barColor = "#cfcfcf";
-var barThickness = 20;
+var barThickness = 1;
 
-var activeMovie = null;
+var seedMovie = null;
 
 var numMoviesSimilarTo = 5;
 
 $(document).ready(function() {
+  initAutoComplete();
+
   //$.ajax({
     //url: topRentalsUrl,
     //dataType: "jsonp",
@@ -32,18 +34,63 @@ $(document).ready(function() {
     attr("width", visWidth).
     attr("height", visSize);
 
-  //vis.append("svg:rect").
-    //attr("x", visWidth/2 - barThickness/2).
-    //attr("y", 0).
-    //attr("height", visSize).
-    //attr("width", barThickness).
-    //attr("fill", barColor);
-  //vis.append("svg:rect").
-    //attr("x", 0).
-    //attr("y", visSize/2 - barThickness/2).
-    //attr("height", barThickness).
-    //attr("width", visWidth).
-    //attr("fill", barColor);
+  vis.append("svg:rect").
+    attr("x", visWidth/2 - barThickness/2).
+    attr("y", 0).
+    attr("height", visSize).
+    attr("width", barThickness).
+    attr("fill", barColor);
+  vis.append("svg:rect").
+    attr("x", 0).
+    attr("y", visSize/2 - barThickness/2).
+    attr("height", barThickness).
+    attr("width", visWidth).
+    attr("fill", barColor);
+  
+  vis.append('svg:text').
+    attr('x', visWidth - 90).
+    attr('y', 10).
+    attr("dy", "1.2em").
+    attr("text-anchor", "left").
+    text("+ Critics");
+  vis.append('svg:text').
+    attr('x', visWidth - 90).
+    attr('y', 40).
+    attr("text-anchor", "left").
+    text("+ Audience");
+  vis.append('svg:text').
+    attr('x', 10).
+    attr('y', 10).
+    attr("dy", "1.2em").
+    attr("text-anchor", "left").
+    text("+ Critics");
+  vis.append('svg:text').
+    attr('x', 10).
+    attr('y', 40).
+    attr("text-anchor", "left").
+    text("- Audience");
+  vis.append('svg:text').
+    attr('x', 10).
+    attr('y', visSize - 50).
+    attr("dy", "1.2em").
+    attr("text-anchor", "left").
+    text("- Critics");
+  vis.append('svg:text').
+    attr('x', 10).
+    attr('y', visSize - 20).
+    attr("text-anchor", "left").
+    text("- Audience");
+  vis.append('svg:text').
+    attr('x', visWidth - 90).
+    attr('y', visSize - 50).
+    attr("dy", "1.2em").
+    attr("text-anchor", "left").
+    text("- Critics");
+  vis.append('svg:text').
+    attr('x', visWidth - 90).
+    attr('y', visSize - 20).
+    attr("text-anchor", "left").
+    text("+ Audience");
 
 
   var movies = [
@@ -58,9 +105,97 @@ $(document).ready(function() {
     setSeedMovie(movies[0]);
   });
 
+
 });
 
+function initAutoComplete() {
+  //$('#search').autocomplete({
+    //source: function(req, resp) {
+      //$.getJSON(
+        //moviesSearchUrl,
+        //{
+          //q: req.term
+        //},
+        //function(data) {
+          //console.log(data);
+        //}
+      //);
+    //},
+    //minLength: 2,
+    //select: function( event, ui ) {
+    //},
+    
+  //});
+
+  var search = $('#search');
+  var searchResults = $('#search_results');
+  
+  searchResults.hide();
+
+  search.keyup(function(event) {
+    searchResults.show();
+    var value = search.val();
+    if (value.length < 3) {
+      return;
+    }
+
+    $.getJSON(
+      moviesSearchUrl,
+      {
+        q: value,
+        page_limit: 10
+      },
+      function(data) {
+        var movies = filterToDVDs(data.movies);
+        renderSearchResults(movies);
+      }
+    )
+  });
+
+  $('body').bind('click', function(e) {
+    if($(e.target).closest('#search_results').length == 0) {
+      searchResults.hide();
+    }
+  });
+
+}
+
+function renderSearchResults(movies) {
+  var search = $('#search');
+  var searchResults = $('#search_results');
+
+  searchResults.empty();
+
+  for (var i = 0; i < movies.length; i += 1) {
+    var movie = movies[i];
+
+    var result = $('<div/>').addClass('result').append(
+      $('<img/>').addClass('poster').attr('src', movie.posters.thumbnail),
+      $('<span/>').addClass('title').text(movie.title)
+    );
+
+    result.click((function(movie) {
+      return function() {
+        setSeedMovie(movie);
+        searchResults.hide();
+        search.val('');
+      };
+    })(movie));
+    
+    searchResults.append(result);
+  };
+}
+
+function filterToDVDs(movies) {
+  return movies.filter(function(movie) {
+    return typeof movie.release_dates.dvd !== "undefined" &&
+      typeof movie.title !== "undefined";
+  });
+}
+
 function setSeedMovie(movie) {
+  clearMoviesFromVis();
+
   appendMovieToVis(
     movie,
     visWidth/2 - moviePosterW/2,
@@ -70,10 +205,15 @@ function setSeedMovie(movie) {
   var promise = getMoviesSimilarTo(movie);
   $.when(getMoviesSimilarTo(movie)).
     then(function(){
-    
+    displayMovieDetails(movie);
     plotMoviesSimilarTo(movie);
-         displayMovieDetails(movie);
-  })
+  });
+
+  seedMovie = movie;
+}
+
+function clearMoviesFromVis() {
+  vis.selectAll('image').remove();
 }
 
 function appendMovieToVis(movie, x, y) {
@@ -84,10 +224,18 @@ function appendMovieToVis(movie, x, y) {
     attr("x", x).
     attr("y", y).
     attr("xlink:href", movie.posters.thumbnail).
-    on("click",
+    on("mouseover",
        function(e) {
          displayMovieDetails(movie);
-       });
+       }).
+         on("mouseout",
+            function(e) {
+              displayMovieDetails(seedMovie);
+            }).
+              on("click",
+                 function(e) {
+                   setSeedMovie(movie);
+                 });
 }
 
 function plotMoviesSimilarTo(movie) {
@@ -115,20 +263,20 @@ function plotMoviesSimilarTo(movie) {
     switch (curMovie.quad) {
       case 1:
         appendMovieToVis(curMovie, x(q1X), y(q1Y));
-        q1X++;
-        break;
+      q1X++;
+      break;
       case 2:
         appendMovieToVis(curMovie, x(q2X), y(q2Y));
-        q2X--;
-        break;
+      q2X--;
+      break;
       case 3:
         appendMovieToVis(curMovie, x(q3X), y(q3Y));
-        q3X--;
-        break;
+      q3X--;
+      break;
       case 4:
         appendMovieToVis(curMovie, x(q4X), y(q4Y));
-        q4X++;
-        break;
+      q4X++;
+      break;
     }
 
   };
@@ -146,37 +294,43 @@ function displayMovieDetails(movie) {
     });
   }
 
+  var links = $('<div/>').addClass('external');
+  if (typeof movie.alternate !== "undefined" &&
+     typeof movie.alternate.imdb !== "undefined") {
+    links.append(
+      $('<a/>').attr(
+        {
+      href: 'http://www.imdb.com/title/tt'+movie.alternate_ids.imdb,
+      target: '_blank',
+    }).append($('<img/>').attr('src', 'img/imdb.png'))
+    );
+  }
+  links.append(
+    $('<a/>').attr(
+      {
+    href: movie.links.alternate,
+    target: '_blank',
+  }).append($('<img/>').attr('src', 'img/rt.png'))
+  );
+
   details.append(
     $('<img/>').addClass('poster').attr('src', movie.posters.profile),
-    
+
     $('<div/>').addClass('col1').append(
       $('<div/>').addClass('title').text(movie.title),
       $('<div/>').addClass('plot').text(movie.synopsis)
     ),
 
     $('<div/>').addClass('col2').append(
-      
+
       $('<div/>').addClass('year').text('Year: ' + movie.year),
 
       $('<div/>').addClass('stats').append(
         $('<div/>').addClass('score').text('Audience Score: ' + movie.ratings.audience_score),
         $('<div/>').addClass('score').text('Critics Score: ' + movie.ratings.critics_score)
       ),
-      
       genres,
-      
-      $('<div/>').addClass('external').append(
-        $('<a/>').attr(
-          {
-            href: 'http://www.imdb.com/title/tt'+movie.alternate_ids.imdb,
-            target: '_blank',
-          }).append($('<img/>').attr('src', 'img/imdb.png')),
-        $('<a/>').attr(
-          {
-            href: movie.links.alternate,
-            target: '_blank',
-          }).append($('<img/>').attr('src', 'img/rt.png'))
-      )
+      links
     )
   );
 }
